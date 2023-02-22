964b7568e0ae85e68d0f6760964fc0cfd80a9b5b
Kristoffer Larsson <lilamoln.koffa@gmail.com>
2023-02-22 11:55:55 +0100

    fixade main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,78 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<String> ovningar = new ArrayList<>();
+        HashMap<String, Integer> userProgram = new HashMap<>();
+        HashMap<String, Integer> userProgress = new HashMap<>();
+        ovningar.add("Bänkpress");
+        ovningar.add("Marklyft");
+        ovningar.add("Knäböj");
+        ovningar.add("Chins");
+        ovningar.add("Curls");
+        ovningar.add("Dips");
+
+        /*
+
+
+        Metoder vi behöver:
+        Visa menyn, läs in användarens val, (avsluta)
+
+        Visa tillgängliga övningar (ovningar)
+
+        * En metod för att lägga till övning och reps i userProgram / sluta lägga till (klar med programmet)
+
+        En metod för att visa userProgram
+
+        * En metod för att lägga till övningar man gjort i hashMap userProgress
+
+        " En metod för att skriva ut userProgress
+
+
+
+
+
+
+
+        Pseudokod
+Start:
+Skapa en lista över tillgängliga övningar
+Skapa en tom lista för användarens träningsprogram
+Skapa en tom HashSet för att lagra användarens träningsframsteg
+
+
+Skapa ett färdigt träningsprogram som ska föreslås för användaren.
+
+Loop:
+Skriv ut menyn med alternativ för användaren
+Läs in användarens val
+Om användaren väljer att avsluta:
+Avsluta programmet
+Om användaren väljer att visa tillgängliga övningar:
+Skriv ut listan över tillgängliga övningar
+Om användaren väljer att skapa ett nytt träningsprogram:
+Skapa en ny lista för det anpassade träningsprogrammet
+Loop:
+Skriv ut listan över tillgängliga övningar
+Läs in användarens val av övning
+Läs in antalet repetitioner för övningen
+Lägg till övningen i det anpassade träningsprogrammet
+Fråga användaren om hen vill lägga till fler övningar
+Om användaren väljer "Nej":
+Bryt loopen
+Lägg till det anpassade träningsprogrammet till användarens träningsprogram
+
+Om användaren väljer att visa sitt träningsprogram:
+Skriv ut listan över övningar i användarens träningsprogram
+Om användaren väljer att registrera sina träningsframsteg:
+Skriv ut en lista över övningar i användarens träningsprogram
+Läs in användarens val av övning att registrera framsteg för
+Läs in antalet repetitioner för övningen
+Lägg till användarens träningsframsteg till HashSet för övningen
+Om användaren väljer att visa sina träningsframsteg:
+Skriv ut en sammanfattning av användarens träningsframsteg
+Slut.
+
+         */
     }
 }
