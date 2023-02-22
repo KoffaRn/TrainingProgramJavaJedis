e7e305695ff8ce41ac4a3b22ec610cffdae56b72
Sandra1887 <93sanjep@molndal.gafe.se>
2023-02-22 14:46:49 +0100

    Sandra Main och Methods

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
         ArrayList<String> ovningar = new ArrayList<>();
@@ -12,70 +12,21 @@ public class Main {
         ovningar.add("Curls");
         ovningar.add("Dips");
 
-        /*
-
-
-        Metoder vi behöver:
-        Visa menyn, läs in användarens val, (avsluta) // Habib
-
-        Visa tillgängliga övningar (ovningar) // Habib
-
-        * En metod för att lägga till övning och reps i userProgram / sluta lägga till (klar med programmet) //Kristoffer
-
-        En metod för att visa userProgram //Ellen
-
-        * En metod för att lägga till övningar man gjort i hashMap userProgress // Sandra
-
-        " En metod för att skriva ut userProgress // Den som blir klar med sin metod först (skriv på discord)
-
-
-
-
-
-
-
-        Pseudokod
-Start:
-Skapa en lista över tillgängliga övningar
-Skapa en tom lista för användarens träningsprogram
-Skapa en tom HashSet för att lagra användarens träningsframsteg
-
-
-Skapa ett färdigt träningsprogram som ska föreslås för användaren.
-
-Loop:
-Skriv ut menyn med alternativ för användaren
-Läs in användarens val
-Om användaren väljer att avsluta:
-Avsluta programmet
-Om användaren väljer att visa tillgängliga övningar:
-Skriv ut listan över tillgängliga övningar
-Om användaren väljer att skapa ett nytt träningsprogram:
-Skapa en ny lista för det anpassade träningsprogrammet
-Loop:
-Skriv ut listan över tillgängliga övningar
-Läs in användarens val av övning
-Läs in antalet repetitioner för övningen
-Lägg till övningen i det anpassade träningsprogrammet
-Fråga användaren om hen vill lägga till fler övningar
-Om användaren väljer "Nej":
-Bryt loopen
-Lägg till det anpassade träningsprogrammet till användarens träningsprogram
-
-Om användaren väljer att visa sitt träningsprogram:
-Skriv ut listan över övningar i användarens träningsprogram
-Om användaren väljer att registrera sina träningsframsteg:
-Skriv ut en lista över övningar i användarens träningsprogram
-Läs in användarens val av övning att registrera framsteg för
-Läs in antalet repetitioner för övningen
-Lägg till användarens träningsframsteg till HashSet för övningen
-Om användaren väljer att visa sina träningsframsteg:
-Skriv ut en sammanfattning av användarens träningsframsteg
-Slut.
-
-         */
-    }
-    static void addExercise(String name, int reps) {
+        var input = new Scanner(System.in);
+        boolean loop = true;
+        while (loop) {
+            Methods.printMenu2();
+            System.out.println("Skriv numret på önskat val");
+            int choice = input.nextInt();
+            if (choice == 1) {
+                Methods.addExercise(input, ovningar, userProgress);
+            } else if (choice == 2) {
+                Methods.removeExercise(input, userProgress);
+            } else if (choice == 3) {
+                loop = false;
+                break;
+            }
+        }
 
     }
 }
---
+++ src/Methods.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Methods {
+
+    public static void printMenu2() {
+        //var input = new Scanner(System.in);
+        //MethodS.printMenu2(input); Meny1 -> gör val 2 "skapa träningsprogram"
+        var menu2 = new ArrayList<String>();
+        menu2.add("Lägg till övningar");
+        menu2.add("Ta bort övningar");
+        menu2.add("Avsluta");
+        for (int i = 0; i < menu2.size(); i++) {
+            System.out.println(menu2.get(i));
+        }
+    }
+    public static void addExercise(Scanner input, List<String> ovningar, Map<String, Integer> userProgress) {
+        System.out.println("Skriv namnet på övningen samt antal reps eller \"avsluta\" för att sluta lägga in");
+        String exerc = input.next();
+        int reps = input.nextInt();
+        if (exerc.toLowerCase().equals("avsluta")) {
+            break;
+        } else if (ovningar.contains(exerc)) {
+            userProgress.put(exerc, reps);
+        } else {
+            System.out.println("Övningen finns inte. Försök igen");
+        }
+    }
+
+    public static void removeExercise(Scanner input, Map<String, Integer> userProgress) {
+        System.out.println("Skriv namnet på övningen du vill ta bort");
+        while (input.hasNext()) {
+            String choiceE = input.next();
+            userProgress.remove(choiceE);
+        }
+    }
+}
